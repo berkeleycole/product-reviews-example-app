e4e66eb2911d7633f385771829a3e0f22043c8ae
Alyssa Hope <alyssa@organikseo.com>
2017-09-28 11:09:34 -0700

    initial commit

---
+++ review-free-for-all-front-end/product-reviews-app/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import {
+  Grid,
+  PageHeader
+} from 'react-bootstrap'
+
+class App extends Component {
+  render() {
+    return (
+      <Grid>
+        <PageHeader>
+          Product Reviews App
+        </PageHeader>
+      </Grid>
+    );
+  }
+}
+
+export default App;
