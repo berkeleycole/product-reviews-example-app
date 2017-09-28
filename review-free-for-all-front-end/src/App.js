5e58d95fa270710e17c0da447c65686302b3b0f9
learn <learn@learn12.local>
2017-09-28 15:41:30 -0700

    Resolve conflict

--- review-free-for-all-front-end/src/App.js
+++ review-free-for-all-front-end/src/App.js
@@ -11,15 +11,6 @@ import styles from './App.css'
 class App extends Component {
   render() {
     return (
-<<<<<<< HEAD
-		<div>
-	      <Grid>
-	        <PageHeader>
-				<h1 className='subtitle'>Product Reviews App</h1>
-	        </PageHeader>
-	      </Grid>
-		</div>
-=======
       <Grid>
         <PageHeader className="class">
           Product Reviews App
@@ -28,7 +19,6 @@ class App extends Component {
         <Index />
         <ProductPage />
       </Grid>
->>>>>>> 5909555788d1a9adf8a2a40b3b7232f2040094e3
     );
   }
 }
