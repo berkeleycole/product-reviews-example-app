23937271c6515293fcb550ab12cc651e428940c4
learn <learn@learn09.local>
2017-09-29 11:57:00 -0700

    create review validations, added CORS, app POST & GET

--- review-free-for-all-back-end/__tests__/app.js
+++ review-free-for-all-back-end/__tests__/app.js
@@ -5,15 +5,9 @@ jest.mock('../models/product')
 jest.mock('../models/review')
 
 describe("App", ()=>{
-  //Products Index Test
-  it("Tests the root path", ()=>{
-    return request(app).get("/").then(response => {
-      expect(response.statusCode).toBe(200)
-    })
-  })
 
   it("Lists products", ()=>{
-    return request(app).get("/product").then(response =>{
+    return request(app).get("/").then(response =>{
       expect(response.statusCode).toBe(200)
 	  expect(response.body.product[0].name).toBe('Pencil')
     })
@@ -21,7 +15,7 @@ describe("App", ()=>{
 
   it("Creates products", ()=>{
     return request(app)
-      .post("/product")
+      .post("/add-product")
       .send({
         name: 'Pencil',
         description: "wood with a pointy end."
@@ -35,7 +29,7 @@ describe("App", ()=>{
 
   it("Validates name when creating product", ()=>{
     return request(app)
-      .post("/product")
+      .post("/add-product")
       .send({
         description: 'wood with a pointy end.'
       })
@@ -49,7 +43,7 @@ describe("App", ()=>{
 
   it("Validates description when creating product", ()=>{
     return request(app)
-      .post("/product")
+      .post("/add-product")
       .send({
         name: 'Pencil',
       })
@@ -62,17 +56,32 @@ describe("App", ()=>{
   })
 
   //Review Tests
-  it("Tests the review path", ()=>{
-    return request(app).get("/review").then(response => {
-      expect(response.statusCode).toBe(200)
-    })
+  it("Creates reviews", ()=>{
+    return request(app)
+      .post("/product/:id")
+      .send({
+        productId: 1,
+        review: "Eraser sucks"
+      })
+      .then(response => {
+        expect(response.statusCode).toBe(201)
+        expect(response.body.review.productId).toBe()
+        expect(response.body.review.review).toBe("Eraser sucks")
+      })
   })
 
-  it("Lists reviews", ()=>{
-    return request(app).get("/review").then(response =>{
-  	expect(response.statusCode).toBe(200)
-  	expect(response.body.review[0].review).toBe('Very sharp!')
-    })
+  it("Validates review when creating review", ()=>{
+	return request(app)
+	  .post("/product/:id")
+	  .send({
+		review: ''
+	  })
+	  .then(response =>{
+		expect(response.statusCode).toBe(400)
+		const error = response.body.errors.validations[0]
+		expect(error.param).toBe('review')
+		expect(error.msg).toBe('Is required')
+	  })
   })
 
 })
--- review-free-for-all-back-end/app.js
+++ review-free-for-all-back-end/app.js
@@ -1,40 +1,23 @@
 var express = require('express');
+var cors = require('cors')
 var bodyParser = require('body-parser');
 var validator = require('express-validator');
 var app = express();
 var Product = require('./models').Product;
+var Review = require('./models').Review;
 
 app.use(express.static('public'))
 app.use(bodyParser.json())
 app.use(validator())
-
-Product.create({
-  name: 'Pencil',
-  description: 'wood with a pointy end.'
-}).then(function(product){
-  console.log(product.get())
-
-  return product.createReview({
-    review: 'Very sharp!',
-  })
-})
-.then(function(review){
-  console.log("New Review", review.get())
-}).catch(function(error){
-  console.log(error)
-})
+app.use(cors())
 
 app.get('/', (req, res) => {
-  res.json({message: 'API Example App'})
-});
-
-app.get('/product', (req, res) => {
-  Product.findAll().then( (product) =>{
+  Product.findAll().then((product) =>{
     res.json({product:product})
   })
 })
 
-app.post('/product', (req, res) => {
+app.post('/add-product', (req, res) => {
 	req.checkBody('name', 'Is required').notEmpty()
 	req.checkBody('description', 'Is required').notEmpty()
   	req.getValidationResult()
@@ -52,6 +35,34 @@ app.post('/product', (req, res) => {
         res.json({errors: {validations: validationErrors.array()}})
 	}
   })
+})
+
+app.post('/product/:id', (req, res) => {
+	req.checkBody('review', 'Is required').notEmpty()
+  	req.getValidationResult()
+    .then((validationErrors) =>{
+  	if(validationErrors.isEmpty()){
+  	Review.create({
+    review: req.body.review,
+}).then((review)=>{
+    res.status(201)
+    res.json({review: review})
+})
+	}else{
+		res.status(400)
+        res.json({errors: {validations: validationErrors.array()}})
+	}
+  })
+})
+
+app.get('/product/:id', (req, res) => {
+	Review.findAll({
+		where: {
+			id: req.params["id"],
+		}
+	}).then((product) => {
+		res.json(product)
+	})
 })
 
 module.exports = app
