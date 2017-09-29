0d67f66aea3f85e7099df7d3519df027203b9775
learn <learn@learn14.local>
2017-09-29 13:55:32 -0700

    added handleChange, handleSubmit

--- review-free-for-all-front-end/src/Pages/Add.js
+++ review-free-for-all-front-end/src/Pages/Add.js
@@ -13,18 +13,20 @@ import {
   HelpBlock
 } from 'react-bootstrap'
 
-class Add extends Component {
 
-  errorsFor(attribute){
-    var errorString = ""
-    if(this.props.errors){
-      const errors = this.props.errors.filter(error => error.param === attribute )
-      if(errors){
-        errorString = errors.map(error => error.msg ).join(", ")
-      }
-    }
-    return errorString === "" ? null : errorString
+const defaultForm = {
+  name: '',
+  description: ''
+}
+
+
+class Add extends Component {
+constructor(props) {
+  super(props)
+  this.state = {
+    form: defaultForm
   }
+}
 
   render() {
     return (
@@ -40,23 +42,48 @@ class Add extends Component {
           }
           <ControlLabel>Product Name</ControlLabel>
           <FormGroup validationState={this.errorsFor('name') && 'error'}>
-            <FormControl type="text" placeholder="Name" />
+            <FormControl type="text" placeholder="Name" name="name"
+            onChange={this.handleChange.bind(this)}/>
             {this.errorsFor('name') &&
                 <HelpBlock id="name-help-block">{this.errorsFor('name')}</HelpBlock>
               }
           </FormGroup>
           <FormGroup validationState={this.errorsFor('description') && 'error'}>
             <ControlLabel>Product Description</ControlLabel>
-            <FormControl componentClass="textarea"></FormControl>
+            <FormControl componentClass="textarea" name="description"
+            onChange={this.handleChange.bind(this)}></FormControl>
               {this.errorsFor('description') &&
                   <HelpBlock id="description-help-block">{this.errorsFor('description')}</HelpBlock>
               }
           </FormGroup>
           <FormControl.Feedback />
-          <Button type="submit" onSubmit={this.props.onSubmit.bind(this)}>Submit</Button>
+          <Button type="submit" onClick={this.handleSubmit.bind(this)}>Submit</Button>
         </form>
       </Grid>
     );
+  }
+
+  handleChange(event){
+    const formState = Object.assign({}, this.state.form)
+    formState[event.target.name] = event.target.value
+    this.setState({form: formState})
+  }
+
+  handleSubmit(event){
+    event.preventDefault()
+    this.props.onSubmit(this.state.form)
+    this.setState({form: defaultForm})
+  }
+
+  errorsFor(attribute){
+    var errorString = ""
+    if(this.props.errors){
+      const errors = this.props.errors.filter(error => error.param === attribute )
+      if(errors){
+        errorString = errors.map(error => error.msg ).join(", ")
+      }
+    }
+    return errorString === "" ? null : errorString
   }
 }
 
